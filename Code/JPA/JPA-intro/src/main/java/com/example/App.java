34c0901cb92e9c0f34739f15a8f4d82922472df6
Nagabhushanam <nagabhushanam.n@revature.com>
2021-07-19 15:51:47 +0530

    JPA/Hibernate intro

---
+++ Code/JPA/JPA-intro/src/main/java/com/example/App.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee();
+		employee.id=1;
+		employee.name="Nag";
+		employee.gender=Gender.MALE; 
+		employee.dob=new Date();
+		employee.empProfile="bla bla bla";
+				
+				
+		
+
+		// Using JPA
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		em.persist(employee);
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
