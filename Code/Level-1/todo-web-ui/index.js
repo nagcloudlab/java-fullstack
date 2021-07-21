310e3076a1c4b37c3fbdbc5a78e8b8f10b38f5d6
Nagabhushanam <nagabhushanam.n@revature.com>
2021-07-21 12:46:21 +0530

    complete toggle

--- Code/Level-1/todo-web-ui/index.js
+++ Code/Level-1/todo-web-ui/index.js
@@ -31,6 +31,12 @@ class TodoService {
   deleteTodo(id) {
     this.todos = this.todos.filter((todo) => todo.id !== id);
   }
+  completeTodo(id) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    console.log(this.todos);
+  }
   getTodos(filter = "ALL") {
     if (filter === "ALL") {
       return this.todos;
@@ -57,10 +63,15 @@ newInpEle.addEventListener("keyup", (e) => {
   renderTodos(service.getTodos("ALL"));
 });
 
-
 todosBodyEle.addEventListener("click", (e) => {
-  const todId = e.target.dataset["id"];
-  service.deleteTodo(Number.parseInt(todId));
+  const todoId = Number.parseInt(e.target.dataset["id"]);
+  const action = e.target.dataset["action"];
+  if (action === "complete") {
+    service.completeTodo(todoId);
+  }
+  if (action === "delete") {
+    service.deleteTodo(todoId);
+  }
   renderTodos(service.getTodos("ALL"));
 });
 
@@ -71,12 +82,14 @@ todosBodyEle.addEventListener("click", (e) => {
 function renderTodos(todos) {
   const trElements = todos.map((todo) => {
     const trEle = `
-                <tr>
-                    <td><input type="checkbox" ${
-                      todo.completed ? "checked" : ""
-                    } /></td>
+                <tr class="${todo.completed ? "bg-success" : ""}">
+                    <td><input data-action="complete" data-id=${
+                      todo.id
+                    } type="checkbox" ${todo.completed ? "checked" : ""} /></td>
                     <td>${todo.title}</td>
-                    <td><button data-id=${todo.id}>delete</button></td>
+                    <td><button data-action="delete" data-id=${
+                      todo.id
+                    }>delete</button></td>
                 </tr>
             `;
     return trEle;
