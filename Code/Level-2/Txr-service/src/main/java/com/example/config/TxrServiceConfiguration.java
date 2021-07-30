655431d523990cdc3c2b19c367dced496da46387
nagabhushanam <nagabhushanamn@gmail.com>
2021-07-30 10:44:55 +0530

    externalizing configuration properties

--- Code/Level-2/Txr-service/src/main/java/com/example/config/TxrServiceConfiguration.java
+++ Code/Level-2/Txr-service/src/main/java/com/example/config/TxrServiceConfiguration.java
@@ -5,24 +5,37 @@ import com.example.repository.JdbcAccountRepository;
 import com.example.service.NEFTTxrService;
 import com.example.service.TxrService;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.example"})
+@PropertySource("classpath:db.properties")
 public class TxrServiceConfiguration {
+
+
+    @Value("${db.driver}") // SPEL
+    private String dbDriverClass;
+    @Value("${db.url}")
+    private String dbUrl;
+    @Value("${db.username}")
+    private String dbUser;
+    @Value("${db.password}")
+    private String dbPassword;
 
     @Bean
     public DataSource dataSource(){
         System.out.println("datasource()");
         BasicDataSource dataSource=new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/accounts_db");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root1234");
+        dataSource.setDriverClassName(dbDriverClass);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUser);
+        dataSource.setPassword(dbPassword);
         dataSource.setMaxActive(3);
         return  dataSource;
     }
